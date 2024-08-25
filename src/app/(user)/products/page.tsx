c204b9661450c8946bbd0607f241af570858abcf
1ureka <summerheadquarters001@gmail.com>
2024-08-25 12:17:13 +0800

    refactor: Update Page component to use MUI components

--- src/app/(user)/products/page.tsx
+++ src/app/(user)/products/page.tsx
@@ -1,46 +1,44 @@
-import ProductCard, {
-  ProductCardSkeleton,
-} from "@/components/(user)/ProductCard";
+import Block from "@/components/Block";
+import { Stack, Typography } from "@mui/material";
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { ProductsSection } from "@/components/(user)/ProductsSection";
 
-import { Suspense } from "react";
-import { cache } from "@/lib/cache";
-
-import { getProducts } from "@/data/table";
-import type { Product } from "@prisma/client";
-
-const productsFetcher = cache(
-  () => getProducts("all"),
-  ["/products", "getProducts", "all"]
-);
-
-export default function ProductsPage() {
+export default function Page() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <Suspense
-        fallback={
-          <>
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-            <ProductCardSkeleton />
-          </>
-        }
-      >
-        <ProductsSuspense fetcher={productsFetcher} />
-      </Suspense>
-    </div>
-  );
-}
+    <Stack gap={4}>
+      <Stack direction="row" alignItems="stretch" gap={4}>
+        <Block
+          variant="contained"
+          color="primary.main"
+          decoration="left"
+          sx={{ width: 1 }}
+        >
+          <Stack
+            gap={1}
+            sx={{ color: "text.primary" }}
+            data-mui-color-scheme="dark"
+          >
+            <Typography variant="h5">Products</Typography>
+            <Typography variant="body2">
+              Browse our collection of products and find the perfect item for
+              you.
+            </Typography>
+          </Stack>
+        </Block>
 
-async function ProductsSuspense({
-  fetcher,
-}: {
-  fetcher: () => Promise<Product[]>;
-}) {
-  await new Promise((resolve) => setTimeout(resolve, Math.random() * 2500));
-  return (await fetcher()).map((product) => (
-    <ProductCard key={product.id} {...product} />
-  ));
+        <Block color="primary.main" decoration="right" sx={{ width: 1 }}>
+          <Typography variant="subtitle1">Order By</Typography>
+
+          <ToggleButtonGroup size="small" value="name" color="primary">
+            <ToggleButton value="name">Name</ToggleButton>
+            <ToggleButton value="popular">Popular</ToggleButton>
+            <ToggleButton value="newest">Newest</ToggleButton>
+            <ToggleButton value="price">Price</ToggleButton>
+          </ToggleButtonGroup>
+        </Block>
+      </Stack>
+
+      <ProductsSection />
+    </Stack>
+  );
 }
