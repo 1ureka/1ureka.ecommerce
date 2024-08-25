79b937dfa0b9459061c19643c26b6e749b775a79
1ureka <summerheadquarters001@gmail.com>
2024-08-25 16:01:06 +0800

    refactor: Update AdminPage component to use MUI components
    The code changes update the AdminPage component to use MUI components for improved styling and consistency. This includes replacing the custom Card, CardHeader, CardTitle, CardContent, and CardFooter components with MUI's Stack, Typography, and Box components. The layout and structure of the component remain the same, but the styling is updated to match the MUI design system. This refactor enhances the overall user experience and makes the codebase more maintainable.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,11 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import Block from "@/components/Block";
 import { getSalesData, getCustomerData, getProductData } from "@/data/table";
 import { formatCurrency, formatNumber } from "@/lib/formatters";
+import { Box, Stack, Typography } from "@mui/material";
 
-export default async function AdminPage() {
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   const [salesData, customerData, productData] = await Promise.all([
     getSalesData(),
     getCustomerData(),
@@ -20,28 +17,52 @@ export default async function AdminPage() {
   const { numberOfActive, numberOfInactive } = productData;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <DashboardCard
-        title="Sales"
-        description={`${formatNumber(numberOfSales)} Orders`}
-      >
-        <p>{formatCurrency(amount)}</p>
-      </DashboardCard>
+    <Box>
+      <Block sx={{ minWidth: 300, width: "fit-content", mb: 4, mx: "auto" }}>
+        <Typography variant="h5" sx={{ textAlign: "center" }}>
+          Overview
+        </Typography>
+      </Block>
 
-      <DashboardCard
-        title="Customers"
-        description={`${formatCurrency(averageOrderValue)} Average Value`}
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: 4,
+        }}
       >
-        <p>{formatNumber(numberOfCustomers)}</p>
-      </DashboardCard>
+        <DashboardCard
+          decoration="left"
+          color="primary.main"
+          variant="contained"
+          title="Sales"
+          description={`${formatNumber(numberOfSales)} Orders`}
+          SlotProps={{ childContainer: { "data-mui-color-scheme": "dark" } }}
+        >
+          <p>{formatCurrency(amount)}</p>
+        </DashboardCard>
 
-      <DashboardCard
-        title="Active Products"
-        description={`${formatNumber(numberOfInactive)} Inactive Products`}
-      >
-        <p>{formatNumber(numberOfActive)}</p>
-      </DashboardCard>
-    </div>
+        <DashboardCard
+          decoration="left"
+          color="primary.main"
+          title="Customers"
+          description={`${formatCurrency(averageOrderValue)} Average Value`}
+        >
+          <p>{formatNumber(numberOfCustomers)}</p>
+        </DashboardCard>
+
+        <DashboardCard
+          decoration="left"
+          color="secondary.main"
+          variant="contained"
+          title="Active Products"
+          description={`${formatNumber(numberOfInactive)} Inactive Products`}
+          SlotProps={{ childContainer: { "data-mui-color-scheme": "dark" } }}
+        >
+          <p>{formatNumber(numberOfActive)}</p>
+        </DashboardCard>
+      </Box>
+    </Box>
   );
 }
 
@@ -49,18 +70,21 @@ function DashboardCard({
   title,
   description,
   children,
+  ...props
 }: {
   title: string;
   description: string;
-  children: React.ReactNode;
-}) {
+} & React.ComponentProps<typeof Block>) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
-      <CardContent>{children}</CardContent>
-    </Card>
+    <Block {...props}>
+      <Stack gap={2} sx={{ color: "text.primary" }}>
+        <Stack gap={1}>
+          <Typography variant="h6">{title}</Typography>
+          <Typography variant="body2">{description}</Typography>
+        </Stack>
+
+        {children}
+      </Stack>
+    </Block>
   );
 }
