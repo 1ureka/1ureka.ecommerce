ac7949c0e2866a259379fe4069b33de6e8d0c48f
1ureka <summerheadquarters001@gmail.com>
2024-08-25 15:59:01 +0800

    refactor: Update Loading component to use MUI CircularProgress
    The code changes update the Loading component to use MUI's CircularProgress component instead of the custom Loader2 component from lucide-react. This change improves the visual appearance and consistency of the loading indicator. The component is also wrapped in a Box component with updated alignment properties for better positioning. This refactor enhances the overall user experience and makes the codebase more maintainable.

--- src/app/admin/loading.tsx
+++ src/app/admin/loading.tsx
@@ -1,9 +1,9 @@
-import { Loader2 } from "lucide-react";
+import { Box, CircularProgress } from "@mui/material";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center">
-      <Loader2 className="size-24 animate-spin" />
-    </div>
+    <Box sx={{ alignSelf: "center", justifySelf: "center" }}>
+      <CircularProgress />
+    </Box>
   );
 }
