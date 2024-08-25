25ea168defcb057c12d9b8c48e26a1e405a3857c
1ureka <summerheadquarters001@gmail.com>
2024-08-25 18:54:10 +0800

    refactor: Update ProductActions component to use MUI components

--- src/components/(admin)/ProductActions.tsx
+++ src/components/(admin)/ProductActions.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import { deleteProduct, toggleAvailability } from "@/lib/actions";
 import { useRouter } from "next/navigation";
 
-import { DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
-import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
-import { deleteProduct, toggleAvailability } from "@/lib/actions";
+import { Divider, IconButton, Menu, MenuItem } from "@mui/material";
+import MoreVertRoundedIcon from "@mui/icons-material/MoreVertRounded";
+import Link from "next/link";
 
 export default function ProductActions({
   id,
@@ -16,11 +17,29 @@ export default function ProductActions({
   isAvailableForPurchase: boolean;
   disabled: boolean;
 }) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
-      <ActiveToggle id={id} isAvailableForPurchase={isAvailableForPurchase} />
-      <DropdownMenuSeparator />
-      <DeleteButton id={id} disabled={disabled} />
+      <IconButton aria-label="actions" onClick={handleClick}>
+        <MoreVertRoundedIcon />
+      </IconButton>
+
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <ActiveToggle id={id} isAvailableForPurchase={isAvailableForPurchase} />
+        <MenuItem component={Link} href={`/admin/products/${id}/edit`}>
+          Edit
+        </MenuItem>
+        <Divider />
+        <DeleteButton id={id} disabled={disabled} />
+      </Menu>
     </>
   );
 }
@@ -36,7 +55,7 @@ function ActiveToggle({
   const router = useRouter();
 
   return (
-    <DropdownMenuItem
+    <MenuItem
       disabled={isPedding}
       onClick={() => {
         startTransition(async () => {
@@ -50,7 +69,7 @@ function ActiveToggle({
         : isAvailableForPurchase
         ? "Deactivate"
         : "Activate"}
-    </DropdownMenuItem>
+    </MenuItem>
   );
 }
 
@@ -59,8 +78,8 @@ function DeleteButton({ id, disabled }: { id: string; disabled: boolean }) {
   const router = useRouter();
 
   return (
-    <DropdownMenuItem
-      variant="destructive"
+    <MenuItem
+      sx={{ color: "error.main" }}
       disabled={disabled || isPedding}
       onClick={() => {
         startTransition(async () => {
@@ -70,6 +89,6 @@ function DeleteButton({ id, disabled }: { id: string; disabled: boolean }) {
       }}
     >
       {isPedding ? "Loading..." : "Delete"}
-    </DropdownMenuItem>
+    </MenuItem>
   );
 }
